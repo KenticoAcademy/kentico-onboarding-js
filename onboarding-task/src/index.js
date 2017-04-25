6da263a8cce328e9b8d6f7c4f331513567e8326b
David Cechak <DavidC@kentico.com>
2017-04-25 13:41:49 +0200

    KA-106 Add thunk middleware & and startup fetch

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './index.css';
 import App from './App.tsx';
 import { itemsReducer } from './reducers/itemsReducer.ts';
+import { fetchItems } from './actions/actionCreators.ts';
 
 const logger = createLogger();
-const store = createStore(itemsReducer, applyMiddleware(logger));
+const store = createStore(itemsReducer, applyMiddleware(logger, thunk));
+
+store.dispatch(fetchItems());
 
 ReactDOM.render(
   <Provider store={store}>
