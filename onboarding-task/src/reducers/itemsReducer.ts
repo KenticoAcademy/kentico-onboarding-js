7fdde9ead2e3f0c731d1de5f9a6cc2ba39ebd501
David Cechak <DavidC@kentico.com>
2017-05-10 19:08:00 +0200

    KA-106 Rewrite itemsReducer to be typed
    Do not use combineReducer due typing
    based on pair review

--- onboarding-task/src/reducers/itemsReducer.ts
+++ onboarding-task/src/reducers/itemsReducer.ts
@@ -1,17 +1,17 @@
-import { combineReducers } from 'redux';
-
 import { itemsDataReducer } from './itemsDataReducer';
 import { itemsFlagReducer } from './itemsFlagReducer';
 import { itemsOrderReducer } from './itemsOrderReducer';
-import { itemsFetchReducer } from './itemsFetchReducer';
+import { itemsIsFetchingReducer } from './itemsFetchReducer';
 import { itemsErrorReducer } from './itemsErrorReducer';
+import { IAppState } from '../stores/IAppState';
+import { IAction } from '../actions/IAction';
 
-const itemsReducer = combineReducers({
-  items: itemsDataReducer,
-  itemsOrder: itemsOrderReducer,
-  itemsFlags: itemsFlagReducer,
-  isFetching: itemsFetchReducer,
-  errors: itemsErrorReducer,
-});
-
-export { itemsReducer };
+export function itemsReducer(prevState: IAppState = {} as IAppState, action: IAction): IAppState {
+  return {
+    items: itemsDataReducer(prevState.items, action),
+    itemsOrder: itemsOrderReducer(prevState.itemsOrder, action),
+    itemsFlags: itemsFlagReducer(prevState.itemsFlags, action),
+    isFetching: itemsIsFetchingReducer(prevState.isFetching, action),
+    errors: itemsErrorReducer(prevState.errors, action),
+  };
+}
