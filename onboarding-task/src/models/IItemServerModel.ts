c21ecc5b3e520d7183e9b986e405db7ad59f812a
David Cechak <DavidC@kentico.com>
2017-05-17 15:02:19 +0200

    KA-106 Change typing of Date, comment reason

--- onboarding-task/src/models/IItemServerModel.ts
+++ onboarding-task/src/models/IItemServerModel.ts
@@ -2,6 +2,7 @@ export interface IItemServerModel {
   readonly id: string;
   readonly ueid: string;
   readonly value: string;
-  readonly created: string;
-  readonly lastUpdated: string;
+  // properties below are not used within the application
+  readonly created: any;
+  readonly lastUpdated: any;
 }
