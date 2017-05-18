0b5bb9f64cfab16d82588ba96aa7a17d7a660af7
David Cechak <DavidC@kentico.com>
2017-05-18 13:49:09 +0200

    Ka-106 Define globaly only necessary types

--- onboarding-task/src/@types/global.d.ts
+++ onboarding-task/src/@types/global.d.ts
@@ -1,7 +1,8 @@
 import { IAction } from '../actions/IAction';
 
+type dispatchAction = (action: IAction) => IAction;
+type dispatchThunk = (thunkAction: ((dispatch: dispatchAction) => Promise<IAction>)) => Promise<IAction>;
+
 declare global {
-  type dispatchAction = (action: IAction) => IAction;
-  type dispatchThunk = (thunkAction: ((dispatch: dispatchAction) => Promise<IAction>)) => Promise<IAction>;
   type Dispatch = dispatchThunk & dispatchAction;
 }
