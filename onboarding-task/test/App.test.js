8417fb8605cb10345b6a358a0c4b3bffee1e8db1
David Cechak <DavidC@kentico.com>
2017-05-18 13:49:54 +0200

    Ka-106 Commect the reason for ignoring a test

--- onboarding-task/test/App.test.js
+++ onboarding-task/test/App.test.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import App from '../src/App.tsx';
 import { rootReducer } from '../src/reducers/rootReducer.ts';
 
+// logs errorMessages into test console output for the inaccessibility of server
 xit('renders without crashing', () => {
   const div = document.createElement('div');
   const logger = createLogger();
-  const store = createStore(rootReducer, applyMiddleware(logger));
+  const store = createStore(rootReducer, applyMiddleware(logger, thunk));
   ReactDOM.render(
     <Provider store={store}>
       <App />
