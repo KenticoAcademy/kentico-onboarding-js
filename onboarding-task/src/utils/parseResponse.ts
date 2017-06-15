2102b2ddc938458b84417eb76d3ff84768cb7ba1
David Cechak <DavidC@kentico.com>
2017-06-15 12:10:40 +0200

    KA-106 Refactor parseResponse
    move status check into then

--- onboarding-task/src/utils/parseResponse.ts
+++ onboarding-task/src/utils/parseResponse.ts
@@ -1,10 +1,9 @@
 export const parseResponse = (errorMessage: string) => (response: Response): Promise<any> => {
-  if (response.ok) {
-    return response.json();
-  }
   return response.json().catch(() =>
     Promise.reject(new Error(response.statusText + ': ' + errorMessage))
-  ).then((data: any) =>
-    Promise.reject(new Error(response.statusText + ': ' + data.modelState.value.toString()))
+  ).then((data: any) => {
+      return response.ok ?
+        data : Promise.reject(new Error(response.statusText + ': ' + data.modelState.value.toString()));
+    }
   );
 };
