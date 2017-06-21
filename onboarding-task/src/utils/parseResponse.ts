b33a17d272904c5681f8b9de1df82baa92e083f8
David Cechak <DavidC@kentico.com>
2017-06-21 16:23:59 +0200

    KA-106 Refactor parseResponse

--- onboarding-task/src/utils/parseResponse.ts
+++ onboarding-task/src/utils/parseResponse.ts
@@ -1,9 +1,6 @@
-export const parseResponse = (errorMessage: string) => (response: Response): Promise<any> => {
-  return response.json().catch(() =>
-    Promise.reject(new Error(response.statusText + ': ' + errorMessage))
-  ).then((data: any) => {
-      return response.ok ?
-        data : Promise.reject(new Error(response.statusText + ': ' + data.modelState.value.toString()));
-    }
-  );
-};
+export const parseResponse = (errorMessage: string) => (response: Response): Promise<any> => response
+  .json()
+  .catch(() => Promise.reject(new Error(response.statusText + ': ' + errorMessage)))
+  .then((data: any) => response.ok
+    ? data
+    : Promise.reject(new Error(response.statusText + ': ' + data.modelState.value.toString())));
