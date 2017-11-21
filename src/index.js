673460f95db3b7cb728780465a0195f2cfe5a55c
Linda Langerova <LindaL@kentico.com>
2017-11-21 14:50:07 +0100

    KA-214 Create store and add Provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import { App } from './App.jsx';
+import items from './utils/reducer';
 
+let store = createStore(items);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app-root')
 );
