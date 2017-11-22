bf6aea8523e0c2cfe4330ec54c5875f8b47d1ab8
Linda Langerova <LindaL@kentico.com>
2017-11-22 12:26:44 +0100

    KA-214 Make default items fill store

--- src/constants/defaultListItems.jsx
+++ src/constants/defaultListItems.jsx
@@ -1,29 +1,10 @@
-import { generateId } from '../utils/generateId';
-import { Item } from '../models/Item';
-import Immutable from 'immutable';
+import { addItem } from '../utils/actionCreators';
 
-const item1 = new Item({
-  id: generateId(),
-  value: 'Make a coffee',
-});
-const item2 = new Item({
-  id: generateId(),
-  value: 'Master React',
-});
-const item3 = new Item({
-  id: generateId(),
-  value: 'Learn Redux',
-});
-const item4 = new Item({
-  id: generateId(),
-  value: 'Help making Draft awesome',
-});
-
-const defaultListItems = Immutable.OrderedMap({
-  [item1.id]: item1,
-  [item2.id]: item2,
-  [item3.id]: item3,
-  [item4.id]: item4,
-});
+const defaultListItems = (store) => {
+  store.dispatch(addItem('Make a coffee'));
+  store.dispatch(addItem('Master React'));
+  store.dispatch(addItem('Learn Redux'));
+  store.dispatch(addItem('Help making Draft awesome'));
+};
 
 export { defaultListItems };
--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { App } from './App.jsx';
 import items from './reducers/items';
+import { addItem } from './utils/actionCreators';
 
 let store = createStore(items);
+
+console.log(store.getState());
 
 ReactDOM.render(
   <Provider store={store}>
