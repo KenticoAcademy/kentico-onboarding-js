ab6811109f6c64ebc8b60e655314b3620135d60b
Radoslav Karlik <RadoslavK@kentico.com>
2017-11-22 15:59:36 +0100

    KA-221 Change list of items to OrderMap of Record

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,34 +1,37 @@
 import React, { PureComponent } from 'react';
+import { Record, OrderedMap } from 'immutable';
 import { ListItem } from './ListItem.jsx';
 import { NewItemForm } from './NewItemForm';
 import { createNewId } from '../utils/createNewId';
+
+const ListItemRecord = Record({ id: '', text: '' });
 
 export class List extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: [],
+      items: OrderedMap(),
     };
   }
 
   onDelete = (itemId) => {
-    const newItems = this.state.items.filter(item => item.id !== itemId);
+    const newItems = this.state.items.delete(itemId);
     this.setState({ items: newItems });
   };
 
   addNewItem = (text) => {
     const id = createNewId();
-    const item = { text, id };
-    const newItems = this.state.items.concat(item);
+    const item = new ListItemRecord({ id, text });
+    const newItems = this.state.items.set(id, item);
 
     this.setState({ items: newItems });
   };
 
-  changeItemText = (id, newText) => {
-    const newItems = this.state.items.map(item => {
-      if (item.id === id) {
-        return { ...item, text: newText };
+  changeItemText = (changedItemId, newText) => {
+    const newItems = this.state.items.map((item, id) => {
+      if (id === changedItemId) {
+        return item.set('text', newText);
       }
       return item;
     });
@@ -37,19 +40,25 @@ export class List extends PureComponent {
   };
 
   render() {
-    const listItems = this.state.items.map((item, index) =>
-      <li
-        className="list-group-item"
-        key={item.id}
-      >
-        <ListItem
-          item={item}
-          number={index + 1}
-          onSave={this.changeItemText}
-          onDelete={this.onDelete}
-        />
-      </li>
-    );
+    let counter = 0;
+
+    const listItems = this.state.items.map((item, id) => {
+      counter++;
+
+      return (
+        <li
+          className="list-group-item"
+          key={id}
+        >
+          <ListItem
+            item={item}
+            number={counter}
+            onSave={this.changeItemText}
+            onDelete={this.onDelete}
+          />
+        </li>
+      );
+    });
 
     return (
       <ol className="list-group">
