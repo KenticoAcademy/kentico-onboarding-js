a9bd8c04f69dc25641ea4e94cf31dc43709c48e5
Linda Langerova <LindaL@kentico.com>
2017-11-30 12:25:28 +0100

    KA-214 Change list to be functional component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,31 +1,35 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { ListItem } from './ListItem';
 import { EditedListItem } from './EditedListItem';
 
-export class List extends PureComponent {
-  render() {
-    const { itemsMap, deleteItem, updateItemText, toggleEditing } = this.props;
+export const List = ({ itemsMap, deleteItem, updateItemText, toggleEditing }) => {
+  return (<div>{
+      itemsMap.valueSeq().map((item, index) => (
+        <div
+          className="list-group-item form-inline"
+          key={item.id}
+        >{index + 1}
+          {'. '}
+          {item.isBeingEdited ?
+            <EditedListItem
+              text={item.text}
+              onSave={(newText) => updateItemText(item.id, newText)}
+              onDelete={() => deleteItem(item.id)}
+              onCancel={() => toggleEditing(item.id)}
+            /> : <ListItem
+              text={item.text}
+              onClick={() => toggleEditing(item.id)}
+            />}
+        </div>
+      ))
+    }</div>
+  );
+};
 
-    return (<div>{
-        itemsMap.valueSeq().map((item, index) => (
-          <div
-            className="list-group-item form-inline"
-            key={item.id}
-          >{index + 1}
-            {'. '}
-            {item.isBeingEdited ?
-              <EditedListItem
-                text={item.text}
-                onSave={(newText) => updateItemText(item.id, newText)}
-                onDelete={() => deleteItem(item.id)}
-                onCancel={() => toggleEditing(item.id)}
-              /> : <ListItem
-                text={item.text}
-                onClick={() => toggleEditing(item.id)}
-              />}
-          </div>
-        ))
-      }</div>
-    );
-  }
-}
+List.propTypes = {
+  itemsMap: PropTypes.object.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  updateItemText: PropTypes.func.isRequired,
+  toggleEditing: PropTypes.func.isRequired,
+};
