2900c3ff9bd131ed863eab6c2502d73d193f87ca
Linda Langerova <LindaL@kentico.com>
2017-12-05 15:21:18 +0100

    KA-214 Symplify List container and remove what's not necessary

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,22 +1,7 @@
 import { connect } from 'react-redux';
 import { List as ListComponent } from '../components/List';
-import {
-  deleteItem,
-  toggleEditing,
-  updateItemText,
-} from '../actions/actionCreators';
 
-function mapStateToProps(state) {
-  return {
-    itemsMap: state.items.byId,
-  };
-}
+const mapStateToProps = (state) => ({ itemsMap: state.items.byId });
 
-const mapDispatchToProps = (dispatch) => ({
-  deleteItem: (id) => dispatch(deleteItem(id)),
-  toggleEditing: (id) => dispatch(toggleEditing(id)),
-  updateItemText: (id, newText) => dispatch(updateItemText(id, newText)),
-});
-
-export const List = connect(mapStateToProps, mapDispatchToProps)(ListComponent);
+export const List = connect(mapStateToProps, null)(ListComponent);
 
