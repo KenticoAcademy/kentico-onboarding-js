19482635d787d11b4213027364a43da46f650fca
Linda Langerova <LindaL@kentico.com>
2017-12-11 17:43:40 +0100

    KA-214 Beautify default items definition

--- src/constants/defaultListItems.js
+++ src/constants/defaultListItems.js
@@ -2,41 +2,21 @@ import { OrderedMap } from 'immutable';
 import { generateId } from '../utils/generateId';
 import { Item } from '../models/Item';
 
-const id1 = generateId();
-const id2 = generateId();
-const id3 = generateId();
-const id4 = generateId();
-
 const defaultListItems = [
-  [
-    id1,
+  'Make a coffee',
+  'Master React',
+  'Learn Redux',
+  'Help making Draft awesome',
+].map(text => {
+  const id = generateId();
+  return [
+    id,
     new Item({
-      id: id1,
-      text: 'Make a coffee',
+      id,
+      text,
     }),
-  ],
-  [
-    id2,
-    new Item({
-      id: id2,
-      text: 'Master React',
-    }),
-  ],
-  [
-    id3,
-    new Item({
-      id: id3,
-      text: 'Learn Redux',
-    }),
-  ],
-  [
-    id4,
-    new Item({
-      id: id4,
-      text: 'Help making Draft awesome',
-    }),
-  ],
-];
+  ];
+});
 
 const defaultStateValues = {
   items: {
