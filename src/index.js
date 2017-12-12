bbf5c1afa9b6373e2f290853344a05ba9c57bf06
Radoslav Karlik <RadoslavK@kentico.com>
2017-12-12 15:18:59 +0100

    KA-222 Utilize redux logger

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import { reducers } from './reducers';
 import { initialState } from './constants/initialState';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import { App } from './App.jsx';
-
-const logger = store => next => action => {
-  console.group(action.type);
-  console.info('dispatching ', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd(action.type);
-  return result;
-};
 
 const store = createStore(reducers, initialState, applyMiddleware(logger));
 
