50c26814800b3e5453e422186af93c675d07b579
Linda Langerova <LindaL@kentico.com>
2017-12-12 16:13:30 +0100

    KA-215 Change to ts

---
+++ src/actions/actionCreators.ts
@@ -0,0 +1,52 @@
+import { actionTypes } from '../constants/actionTypes';
+import { IAction } from './IAction';
+
+export const addItem = (id: string, text: string): IAction => {
+  return {
+    type: actionTypes.ADD_ITEM,
+    payload: {
+      text,
+      id,
+    },
+  };
+};
+
+export const deleteItem = (id: string): IAction => {
+  return {
+    type: actionTypes.DELETE_ITEM,
+    payload: { id },
+  };
+};
+
+export const toggleEditing = (id: string): IAction => {
+  return {
+    type: actionTypes.TOGGLE_EDITING,
+    payload: { id },
+  };
+};
+
+export const updateItemText = (id: string, newText: string): IAction => {
+  return {
+    type: actionTypes.UPDATE_ITEM_TEXT,
+    payload: {
+      id,
+      newText,
+    },
+  };
+};
+
+export const updateNewItemText = (newItemText: string): IAction => {
+  return {
+    type: actionTypes.UPDATE_NEW_ITEM_TEXT,
+    payload: { newItemText },
+  };
+};
+
+export const textUpdateChange = (id: string, updatedText: string): IAction => {
+  return {
+    type: actionTypes.TEXT_UPDATE_CHANGE,
+    payload: {
+      id,
+      updatedText },
+  };
+};
