f23a39eb17b3ad9ddce08d11eba826e64bc727a4
Radoslav Karlik <RadoslavK@kentico.com>
2017-12-19 15:50:02 +0100

    KA-222 Add memoization on item keys array

--- src/containers/Items.jsx
+++ src/containers/Items.jsx
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux';
 import { Items as ItemsComponent } from '../components/Items';
+import { keySeqToArray } from '../utils/keySeqToArray';
 
 const mapStateToProps = state => ({
-  itemIds: state.list.items.keySeq().toArray(),
+  itemIds: keySeqToArray(state.list.items.keySeq()),
 });
 
 export const Items = connect(
--- src/containers/ListItem.jsx
+++ src/containers/ListItem.jsx
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import moize from 'moize';
 import PropTypes from 'prop-types';
 import { ListItem as ListItemComponent } from '../components/ListItem';
 
