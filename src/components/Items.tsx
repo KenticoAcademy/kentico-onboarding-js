029644ad22610f4a6070d8bf797800f618f5d8a5
Radoslav Karlik <RadoslavK@kentico.com>
2018-01-08 12:54:43 +0100

    KA-259 Make items component fetch data from server

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -2,31 +2,47 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { ListItem } from '../containers/ListItem';
 import { Guid } from '../models/Guid';
-
-const propTypes = {
-  itemIds: PropTypes.arrayOf(PropTypes.string),
-};
+import { FetchItemsState } from '../models/FetchItemsState';
 
 export interface IItemsDataProps {
   readonly itemIds: Guid[];
+  readonly fetchItemsState: FetchItemsState;
 }
 
-const Items: React.SFC<IItemsDataProps> = ({ itemIds }) => (
-  <div>
-    {itemIds.map((itemId, index) => (
-      <li
-        className="list-group-item"
-        key={itemId}
-      >
-        <ListItem
-          itemId={itemId}
-          itemNumber={index + 1}
-        />
-      </li>))}
-  </div>
-);
+export interface IItemsCallbackProps {
+  readonly fetchItems: (uri: string) => void;
+}
 
-Items.displayName = 'Items';
-Items.propTypes = propTypes;
+interface IIItemsProps extends IItemsDataProps, IItemsCallbackProps {}
 
-export { Items };
+export class Items extends React.PureComponent<IIItemsProps> {
+  static displayName = 'Items';
+
+  static propTypes = {
+    itemIds: PropTypes.arrayOf(PropTypes.string),
+    fetchItems: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    if (this.props.fetchItemsState === FetchItemsState.INITIAL) {
+      this.props.fetchItems('/api/v1/listItems');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.itemIds.map((itemId, index) => (
+          <li
+            className="list-group-item"
+            key={itemId}
+          >
+            <ListItem
+              itemId={itemId}
+              itemNumber={index + 1}
+            />
+          </li>))}
+      </div>
+    );
+  }
+}
--- src/containers/Items.tsx
+++ src/containers/Items.tsx
@@ -1,13 +1,22 @@
 import { ComponentClass } from 'react';
 import { connect } from 'react-redux';
-import { IItemsDataProps, Items as ItemsComponent } from '../components/Items';
+import { IItemsCallbackProps, IItemsDataProps, Items as ItemsComponent } from '../components/Items';
 import { IAppState } from '../models/IAppState';
 import { keySeqToArray } from '../utils/keySeqToArray';
+import { Dispatch } from 'redux';
+import { fetchItems } from '../actions/thunk';
+import { IAction } from '../models/IAction';
 
 const mapStateToProps = (state: IAppState): IItemsDataProps => ({
   itemIds: keySeqToArray(state.list.items.keySeq()),
+  fetchItemsState: state.list.fetchItemsState,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<IAction>): IItemsCallbackProps => ({
+  fetchItems: (uri: string) => fetchItems(uri)(dispatch),
 });
 
 export const Items: ComponentClass = connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(ItemsComponent);
