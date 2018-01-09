9736670e1ae382aa69f57abcea3afb4a73aa4464
Linda Langerova <LindaL@kentico.com>
2018-01-09 07:19:22 +0100

    KA-215 Remove 'any' type

--- src/selectors/selectItemIdsMemoized.ts
+++ src/selectors/selectItemIdsMemoized.ts
@@ -1,9 +1,10 @@
 import { OrderedMap, Seq } from 'immutable';
 import { ItemId } from '../models/ItemId';
+import { Item } from '../models/Item';
 const memoize = require('fast-memoize');
 
 const memoizeIds = memoize((ids: Seq.Indexed<ItemId>) => ids);
 
-export const selectItemIdsMemoized = (idsMap: OrderedMap<ItemId, Record<ItemId, any>>): Seq.Indexed<ItemId>  =>
+export const selectItemIdsMemoized = (idsMap: OrderedMap<ItemId, Item>): Seq.Indexed<ItemId>  =>
   memoizeIds(idsMap.keySeq());
 
