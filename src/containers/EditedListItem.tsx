8f377c8d2246b438f109ed6bc1490f3e7b2e0dcf
Linda Langerova <LindaL@kentico.com>
2018-01-09 11:02:01 +0100

    KA-215 Rename file

--- src/containers/EditedListItem.tsx
+++ src/containers/EditedListItem.tsx
@@ -1,34 +1,26 @@
 import { connect } from 'react-redux';
-import { EditedListItem as EditedListItemComponent, IEditedListItemCallbackProps, IEditedListItemDataProps } from '../components/EditedListItem';
+import { EditedListItem as EditedListItemComponent } from '../components/EditedListItem';
 import {
   deleteItem,
   toggleEditing,
   updateItemText,
   textUpdateChange,
-} from '../actions/actionCreators';
-import { IAppState } from '../stores/IAppState';
-import { ComponentClass } from 'react';
-import { IItem } from '../models/Item';
-import { ItemId } from '../models/ItemId';
+} from '../actions/actionCreators.ts';
 
-interface IEditListItemContainerProps {
-  itemId: ItemId;
-}
-
-function mapStateToProps(state: IAppState, { itemId }: IEditListItemContainerProps): IEditedListItemDataProps {
-  const item: IItem = state.items.byId.get(itemId);
+function mapStateToProps(state, { itemId }) {
+  const item = state.items.byId.get(itemId);
   return {
     itemText: item.text,
     textUpdate: item.textUpdate,
   };
 }
 
-const mapDispatchToProps = (dispatch: Function, { itemId }: IEditListItemContainerProps): IEditedListItemCallbackProps => ({
+const mapDispatchToProps = (dispatch, { itemId, updatedText }) => ({
   onDelete: () => dispatch(deleteItem(itemId)),
   onCancel: () => dispatch(toggleEditing(itemId)),
-  onSave: () => dispatch(updateItemText(itemId)),
-  textUpdateChange: (text: string) => dispatch(textUpdateChange(itemId, text)),
+  onSave: () => dispatch(updateItemText(itemId, updatedText)),
+  textUpdateChange: (text) => dispatch(textUpdateChange(itemId, text)),
 });
 
-export const EditedListItem: ComponentClass<IEditListItemContainerProps>  = connect(mapStateToProps, mapDispatchToProps)(EditedListItemComponent);
+export const EditedListItem = connect(mapStateToProps, mapDispatchToProps)(EditedListItemComponent);
 
