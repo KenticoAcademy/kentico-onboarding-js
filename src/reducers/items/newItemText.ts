5fb999cd3f3106b65bba4d23ff7724c751575f6d
Linda Langerova <LindaL@kentico.com>
2018-01-09 10:31:07 +0100

    KA-215 Make all parts working in ts

--- src/reducers/items/newItemText.ts
+++ src/reducers/items/newItemText.ts
@@ -1,15 +1,15 @@
 import {
-  ADD_ITEM,
-  UPDATE_NEW_ITEM_TEXT,
-} from '../../constants/actionTypes.ts';
+  actionTypes,
+} from '../../constants/actionTypes';
+import { IAction } from '../../actions/IAction';
 
-const DEFAULT_VALUE = '';
+const DEFAULT_VALUE: string = '';
 
-export const newItemText = (state = DEFAULT_VALUE, action) => {
+export const newItemText = (state = DEFAULT_VALUE, action: IAction): string => {
   switch (action.type) {
-    case UPDATE_NEW_ITEM_TEXT:
+    case actionTypes.UPDATE_NEW_ITEM_TEXT:
       return action.payload.newItemText;
-    case ADD_ITEM:
+    case actionTypes.ADD_ITEM:
       return DEFAULT_VALUE;
     default:
       return state;
