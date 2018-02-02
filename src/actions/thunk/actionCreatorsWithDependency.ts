323e016881b4ac44d6a7a543d6e4eb1e7a6310fe
Radoslav Karlik <RadoslavK@kentico.com>
2018-02-02 15:11:52 +0100

    KA-259 Add dispatching of messages

--- src/actions/thunk/actionCreatorsWithDependency.ts
+++ src/actions/thunk/actionCreatorsWithDependency.ts
@@ -1,16 +1,33 @@
 import { IAction } from '../../models/IAction';
 import { IListItem } from '../../models/IListItem';
 import { Dispatch } from 'redux';
-import { addNewItem, cancelItemChanges, deleteItem, receiveItems, requestItems, openItemForEditing, saveItemChanges } from '../actionCreators';
+import {
+  addNewItem, cancelItemChanges, deleteItem, fetchFailed, notifyError, notifySuccess,
+  openItemForEditing, receiveItems, requestItems,
+  saveItemChanges
+} from '../actionCreators';
 import { Guid } from '../../models/Guid';
 import { ListItem } from '../../models/ListItem';
+
+const handleErrors = (response: any) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  return response;
+};
 
 export const fetchItemsFactory = (fetch: any) => (uri: string) => (dispatch: Dispatch<IAction>) => {
   dispatch(requestItems(uri));
 
   return fetch(uri)
+    .then(handleErrors)
     .then((res: any) => res.json())
-    .then((items: IListItem[]) => dispatch(receiveItems(items)));
+    .then((items: IListItem[]) => dispatch(receiveItems(items)))
+    .catch((err: Error) => {
+      dispatch(notifyError('Items failed to load.'));
+      dispatch(fetchFailed(err));
+    });
 };
 
 export const postItemFactory = (fetch: any) => (uri: string, text: string) => (dispatch: Dispatch<IAction>) => {
@@ -29,8 +46,13 @@ export const postItemFactory = (fetch: any) => (uri: string, text: string) => (d
       body: JSON.stringify(newItem),
     }
   )
+    .then(handleErrors)
     .then((res: any) => res.json())
-    .then((returnedItem: IListItem) => dispatch(addNewItem(returnedItem)));
+    .then((returnedItem: IListItem) => {
+      dispatch(notifySuccess('Item was created.'));
+      dispatch(addNewItem(returnedItem));
+    })
+    .catch(() => dispatch(notifyError('Item failed to create.')));
 };
 
 export const deleteItemFactory = (fetch: any) => (uri: string, id: Guid) => (dispatch: Dispatch<IAction>) =>
@@ -40,7 +62,12 @@ export const deleteItemFactory = (fetch: any) => (uri: string, id: Guid) => (dis
       method: 'DELETE',
     },
   )
-    .then(() => dispatch(deleteItem(id)));
+    .then(handleErrors)
+    .then(() => {
+      dispatch(notifySuccess('Item was deleted.'));
+      dispatch(deleteItem(id));
+    })
+    .catch(() => dispatch(notifyError('Item failed to delete.')));
 
 export const cancelItemFactory = (fetch: any) => (uri: string, item: ListItem) => (dispatch: Dispatch<IAction>) => {
   const { id } = item;
@@ -59,7 +86,9 @@ export const cancelItemFactory = (fetch: any) => (uri: string, item: ListItem) =
       body: JSON.stringify(updatedItem),
     },
   )
-    .then(() => dispatch(cancelItemChanges(id)));
+    .then(handleErrors)
+    .then(() => dispatch(cancelItemChanges(id)))
+    .catch(() => dispatch(notifyError('Item failed to cancel.')));
 };
 
 export const openItemFactory = (fetch: any) => (uri: string, item: ListItem) => (dispatch: Dispatch<IAction>) => {
@@ -79,7 +108,9 @@ export const openItemFactory = (fetch: any) => (uri: string, item: ListItem) => 
       body: JSON.stringify(updatedItem),
     },
   )
-    .then(() => dispatch(openItemForEditing(id)));
+    .then(handleErrors)
+    .then(() => dispatch(openItemForEditing(id)))
+    .catch(() => dispatch(notifyError('Item failed to open.')));
 };
 
 export const saveNewTextFactory = (fetch: any) => (uri: string, item: ListItem, text: string) => (dispatch: Dispatch<IAction>) => {
@@ -100,5 +131,10 @@ export const saveNewTextFactory = (fetch: any) => (uri: string, item: ListItem, 
       body: JSON.stringify(updatedItem),
     },
   )
-    .then(() => dispatch(saveItemChanges(item.id, text)))
+    .then(handleErrors)
+    .then(() => {
+      dispatch(notifySuccess('Item text was updated.'));
+      dispatch(saveItemChanges(item.id, text));
+    })
+    .catch(() => dispatch(notifyError('Item failed to update.')));
 };
