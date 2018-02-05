c99ffb144e9bd5f4dbcd0ff731dbe06db318d3b8
Linda Langerova <LindaL@kentico.com>
2018-02-05 16:58:04 +0100

    KA-215 Rename id generating function prop

--- src/actions/addItem.ts
+++ src/actions/addItem.ts
@@ -1,13 +1,13 @@
 import { IAction } from './IAction';
 import { actionTypes } from '../constants/actionTypes';
 
-export const addItemFactory: (idFunc: () => string) => ((text: string) => IAction) =
-  idFunc =>
+export const addItemFactory: (idGenerator: () => string) => ((text: string) => IAction) =
+  idGenerator =>
     text => ({
       type: actionTypes.ADD_ITEM,
       payload: {
         text,
-        id: idFunc(),
+        id: idGenerator(),
       },
     });
 
