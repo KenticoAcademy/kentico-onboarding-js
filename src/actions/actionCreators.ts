5e2a6c3970e8bc13814952eead343b6e782df38f
Linda Langerova <LindaL@kentico.com>
2018-02-05 16:55:59 +0100

    KA-215 Shorten functions to be lambdas

--- src/actions/actionCreators.ts
+++ src/actions/actionCreators.ts
@@ -2,39 +2,30 @@ import { actionTypes } from '../constants/actionTypes';
 import { IAction } from './IAction';
 import { ItemId } from '../models/ItemId';
 
-export const deleteItem = (id: ItemId): IAction => {
-  return {
-    type: actionTypes.DELETE_ITEM,
-    payload: { id },
-  };
-};
+export const deleteItem = (id: ItemId): IAction => ({
+  type: actionTypes.DELETE_ITEM,
+  payload: {id},
+});
 
-export const toggleEditing = (id: ItemId): IAction => {
-  return {
-    type: actionTypes.TOGGLE_EDITING,
-    payload: { id },
-  };
-};
+export const toggleEditing = (id: ItemId): IAction => ({
+  type: actionTypes.TOGGLE_EDITING,
+  payload: {id},
+});
 
-export const updateItemText = (id: ItemId): IAction => {
-  return {
-    type: actionTypes.UPDATE_ITEM,
-    payload: { id },
-  };
-};
+export const updateItemText = (id: ItemId): IAction => ({
+  type: actionTypes.UPDATE_ITEM,
+  payload: {id},
+});
 
-export const updateNewItemText = (newItemText: string): IAction => {
-  return {
-    type: actionTypes.UPDATE_NEW_ITEM,
-    payload: { newItemText },
-  };
-};
+export const updateNewItemText = (newItemText: string): IAction => ({
+  type: actionTypes.UPDATE_NEW_ITEM,
+  payload: {newItemText},
+});
 
-export const textUpdateChange = (id: ItemId, updatedText: string): IAction => {
-  return {
-    type: actionTypes.EDIT_ITEM_TEXT,
-    payload: {
-      id,
-      updatedText },
-  };
-};
+export const textUpdateChange = (id: ItemId, updatedText: string): IAction => ({
+  type: actionTypes.EDIT_ITEM_TEXT,
+  payload: {
+    id,
+    updatedText
+  },
+});
