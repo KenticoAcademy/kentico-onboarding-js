a511d77482aab9b47e9800aa87bcf16f6e1d85c3
Radoslav Karlik <RadoslavK@kentico.com>
2018-02-06 12:29:54 +0100

    KA-259 Add tests for post item async action

--- test/actions/thunk/actionCreatorsWithDependency.test.ts
+++ test/actions/thunk/actionCreatorsWithDependency.test.ts
@@ -1,4 +1,4 @@
-import { fetchItemsFactory } from '../../../src/actions/thunk/actionCreatorsWithDependency';
+import { fetchItemsFactory, postItemFactory } from '../../../src/actions/thunk/actionCreatorsWithDependency';
 import Mock = jest.Mock;
 import { IListItem } from '../../../src/models/IListItem';
 import { ListItem } from '../../../src/models/ListItem';
@@ -72,5 +72,42 @@ describe('fetchItems will call dispatch with', () => {
     const dispatchableFetchItems = fetchItems(uri);
 
     return assertThatDispatchWasCalledWithActions(dispatchableFetchItems, dispatch, expectedActions);
+  });
+});
+
+describe('postItem will call dispatch with', () => {
+  it('notify success and add new item actions', () => {
+    const expectedActions = [
+      'notifySuccess',
+      'addNewItem',
+    ];
+    const uri = '';
+    const text = '';
+    const createdItem: IListItem = new ListItem({});
+    const fetch = fetchReturnsOkResponseFactory(createdItem);
+    const notifySuccess = jest.fn(() => expectedActions[0]);
+    const addNewItem = jest.fn(() => expectedActions[1]);
+    const postItem = postItemFactory(fetch)(addNewItem)(notifySuccess)(fakeFunction)(fakeFunction)(handleErrors);
+    const dispatch = dispatchFactory();
+
+    const dispatchablePostItem = postItem(uri, text);
+
+    return assertThatDispatchWasCalledWithActions(dispatchablePostItem, dispatch, expectedActions);
+  });
+
+  it('notify error action', () => {
+    const expectedActions = [
+      'notifyError',
+    ];
+    const uri = '';
+    const text = '';
+    const fetch = fetchAlwaysFailFactory();
+    const notifyError = jest.fn(() => expectedActions[0]);
+    const postItem = postItemFactory(fetch)(fakeFunction)(fakeFunction)(notifyError)(fakeFunction)(handleErrors);
+    const dispatch = dispatchFactory();
+
+    const dispatchablePostItem = postItem(uri, text);
+
+    return assertThatDispatchWasCalledWithActions(dispatchablePostItem, dispatch, expectedActions);
   });
 });
