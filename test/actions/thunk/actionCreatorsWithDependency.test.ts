aaec1e5ad747c0c2511ed95eb304a5baf5943d09
Radoslav Karlik <RadoslavK@kentico.com>
2018-02-06 12:47:17 +0100

    KA-259 Add tests for delete item async action

--- test/actions/thunk/actionCreatorsWithDependency.test.ts
+++ test/actions/thunk/actionCreatorsWithDependency.test.ts
@@ -1,4 +1,4 @@
-import { fetchItemsFactory, postItemFactory } from '../../../src/actions/thunk/actionCreatorsWithDependency';
+import { fetchItemsFactory, postItemFactory, deleteItemFactory } from '../../../src/actions/thunk/actionCreatorsWithDependency';
 import Mock = jest.Mock;
 import { IListItem } from '../../../src/models/IListItem';
 import { ListItem } from '../../../src/models/ListItem';
@@ -109,5 +109,41 @@ describe('postItem will call dispatch with', () => {
     const dispatchablePostItem = postItem(uri, text);
 
     return assertThatDispatchWasCalledWithActions(dispatchablePostItem, dispatch, expectedActions);
+  });
+});
+
+describe('deleteItem will call dispatch with', () => {
+  it('notify success and delete item actions', () => {
+    const expectedActions = [
+      'notifySuccess',
+      'deleteItem',
+    ];
+    const uri = '';
+    const id = '';
+    const fetch = fetchReturnsOkResponseFactory();
+    const notifySuccess = jest.fn(() => expectedActions[0]);
+    const deleteItemAction = jest.fn(() => expectedActions[1]);
+    const deleteItem = deleteItemFactory(fetch)(deleteItemAction)(notifySuccess)(fakeFunction)(fakeFunction)(handleErrors);
+    const dispatch = dispatchFactory();
+
+    const dispatchableDeleteItem = deleteItem(uri, id);
+
+    return assertThatDispatchWasCalledWithActions(dispatchableDeleteItem, dispatch, expectedActions);
+  });
+
+  it('notify error action', () => {
+    const expectedActions = [
+      'notifyError',
+    ];
+    const uri = '';
+    const id = '';
+    const fetch = fetchAlwaysFailFactory();
+    const notifyError = jest.fn(() => expectedActions[0]);
+    const deleteItem = deleteItemFactory(fetch)(fakeFunction)(fakeFunction)(notifyError)(fakeFunction)(handleErrors);
+    const dispatch = dispatchFactory();
+
+    const dispatchableDeleteItem = deleteItem(uri, id);
+
+    return assertThatDispatchWasCalledWithActions(dispatchableDeleteItem, dispatch, expectedActions);
   });
 });
