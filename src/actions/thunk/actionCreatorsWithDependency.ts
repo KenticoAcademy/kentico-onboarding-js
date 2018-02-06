a9c7de9034c5dd16c9cfff1a432b2524131b530d
Radoslav Karlik <RadoslavK@kentico.com>
2018-02-06 12:11:35 +0100

    KA-259 Modify async action creators to use dependency injection

--- src/actions/thunk/actionCreatorsWithDependency.ts
+++ src/actions/thunk/actionCreatorsWithDependency.ts
@@ -1,167 +1,201 @@
 import { IAction } from '../../models/IAction';
 import { IListItem } from '../../models/IListItem';
 import { Dispatch } from 'redux';
-import {
-  addNewItem, cancelItemChanges, deleteItem, fetchFailed, notifyError, notifySuccess,
-  openItemForEditing, receiveItems,
-  registerAction, requestItems, saveItemChanges
-} from '../actionCreators';
 import { Guid } from '../../models/Guid';
 import { ListItem } from '../../models/ListItem';
 
-const handleErrors = (response: any) => {
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
+export const fetchItemsFactory =
+  (fetch: any) =>
+  (requestItems: (uri: string) => IAction) =>
+  (receiveItems: (items: IListItem[]) => IAction) =>
+  (fetchFailed: (err: Error) => IAction) =>
+  (notifyError: (message: string) => IAction) =>
+  (registerAction: (action: () => void) => IAction) =>
+  (handleErrors: <T>(response: T) => T) =>
+  (uri: string) =>
+  (dispatch: Dispatch<IAction>) => {
+    const action = () => {
+      dispatch(requestItems(uri));
 
-  return response;
-};
+      return fetch(uri)
+        .then(handleErrors)
+        .then((res: any) => res.json())
+        .then((items: IListItem[]) => dispatch(receiveItems(items)))
+        .catch((err: Error) => {
+          dispatch(notifyError('Items failed to load.'));
+          dispatch(fetchFailed(err));
+        });
+    };
 
-export const fetchItemsFactory = (fetch: any) => (uri: string) => (dispatch: Dispatch<IAction>) => {
-  const action = () => {
-    dispatch(requestItems(uri));
+    dispatch(registerAction(action));
 
-    return fetch(uri)
+    return action();
+  };
+
+export const postItemFactory =
+  (fetch: any) =>
+  (addNewItem: (item: IListItem) => IAction) =>
+  (notifySuccess: (message: string) => IAction) =>
+  (notifyError: (message: string) => IAction) =>
+  (registerAction: (action: () => void) => IAction) =>
+  (handleErrors: <T>(response: T) => T) =>
+  (uri: string, text: string) =>
+  (dispatch: Dispatch<IAction>) => {
+    const newItem = {
+      isBeingEdited: false,
+      text,
+    };
+
+    const action = () => fetch(
+      uri,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newItem),
+      }
+    )
       .then(handleErrors)
       .then((res: any) => res.json())
-      .then((items: IListItem[]) => dispatch(receiveItems(items)))
-      .catch((err: Error) => {
-        dispatch(notifyError('Items failed to load.'));
-        dispatch(fetchFailed(err));
-      });
+      .then((returnedItem: IListItem) => {
+        dispatch(notifySuccess('Item was created.'));
+        dispatch(addNewItem(returnedItem));
+      })
+      .catch(() => dispatch(notifyError('Item failed to create.')));
+
+    dispatch(registerAction(action));
+
+    return action();
   };
 
-  dispatch(registerAction(action));
-
-  return action();
-};
-
-export const postItemFactory = (fetch: any) => (uri: string, text: string) => (dispatch: Dispatch<IAction>) => {
-  const newItem = {
-    isBeingEdited: false,
-    text,
-  };
-
-  const action = () => fetch(
-    uri,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+export const deleteItemFactory =
+  (fetch: any) =>
+  (deleteItem: (id: Guid) => IAction) =>
+  (notifySuccess: (message: string) => IAction) =>
+  (notifyError: (message: string) => IAction) =>
+  (registerAction: (action: () => void) => IAction) =>
+  (handleErrors: <T>(response: T) => T) =>
+  (uri: string, id: Guid) =>
+  (dispatch: Dispatch<IAction>) => {
+    const action = () => fetch(
+      uri + id,
+      {
+        method: 'DELETE',
       },
-      body: JSON.stringify(newItem),
-    }
-  )
-    .then(handleErrors)
-    .then((res: any) => res.json())
-    .then((returnedItem: IListItem) => {
-      dispatch(notifySuccess('Item was created.'));
-      dispatch(addNewItem(returnedItem));
-    })
-    .catch(() => dispatch(notifyError('Item failed to create.')));
+    )
+      .then(handleErrors)
+      .then(() => {
+        dispatch(notifySuccess('Item was deleted.'));
+        dispatch(deleteItem(id));
+      })
+      .catch(() => dispatch(notifyError('Item failed to delete.')));
 
-  dispatch(registerAction(action));
+    dispatch(registerAction(action));
 
-  return action();
-};
-
-export const deleteItemFactory = (fetch: any) => (uri: string, id: Guid) => (dispatch: Dispatch<IAction>) => {
-  const action = () => fetch(
-    uri + id,
-    {
-      method: 'DELETE',
-    },
-  )
-    .then(handleErrors)
-    .then(() => {
-      dispatch(notifySuccess('Item was deleted.'));
-      dispatch(deleteItem(id));
-    })
-    .catch(() => dispatch(notifyError('Item failed to delete.')));
-
-  dispatch(registerAction(action));
-
-  return action();
-};
-
-export const cancelItemFactory = (fetch: any) => (uri: string, item: ListItem) => (dispatch: Dispatch<IAction>) => {
-  const { id } = item;
-  const updatedItem = {
-    isBeingEdited: false,
-    id,
+    return action();
   };
 
-  const action = () => fetch(
-    uri + id,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
+export const cancelItemFactory =
+  (fetch: any) =>
+  (cancelItemChanges: (id: Guid) => IAction) =>
+  (notifyError: (message: string) => IAction) =>
+  (registerAction: (action: () => void) => IAction) =>
+  (handleErrors: <T>(response: T) => T) =>
+  (uri: string, item: ListItem) =>
+  (dispatch: Dispatch<IAction>) => {
+    const { id } = item;
+    const updatedItem = {
+      isBeingEdited: false,
+      id,
+    };
+
+    const action = () => fetch(
+      uri + id,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedItem),
       },
-      body: JSON.stringify(updatedItem),
-    },
-  )
-    .then(handleErrors)
-    .then(() => dispatch(cancelItemChanges(id)))
-    .catch(() => dispatch(notifyError('Item failed to cancel.')));
+    )
+      .then(handleErrors)
+      .then(() => dispatch(cancelItemChanges(id)))
+      .catch(() => dispatch(notifyError('Item failed to cancel.')));
 
-  dispatch(registerAction(action));
+    dispatch(registerAction(action));
 
-  return action();
-};
-
-export const openItemFactory = (fetch: any) => (uri: string, item: ListItem) => (dispatch: Dispatch<IAction>) => {
-  const { id } = item;
-  const updatedItem = {
-    isBeingEdited: true,
-    id,
+    return action();
   };
 
-  const action = () => fetch(
-    uri + id,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
+export const openItemFactory =
+  (fetch: any) =>
+  (openItemForEditing: (id: Guid) => IAction) =>
+  (notifyError: (message: string) => IAction) =>
+  (registerAction: (action: () => void) => IAction) =>
+  (handleErrors: <T>(response: T) => T) =>
+  (uri: string, item: ListItem) =>
+  (dispatch: Dispatch<IAction>) => {
+    const { id } = item;
+    const updatedItem = {
+      isBeingEdited: true,
+      id,
+    };
+
+    const action = () => fetch(
+      uri + id,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedItem),
       },
-      body: JSON.stringify(updatedItem),
-    },
-  )
-    .then(handleErrors)
-    .then(() => dispatch(openItemForEditing(id)))
-    .catch(() => dispatch(notifyError('Item failed to open.')));
+    )
+      .then(handleErrors)
+      .then(() => dispatch(openItemForEditing(id)))
+      .catch(() => dispatch(notifyError('Item failed to open.')));
 
-  dispatch(registerAction(action));
+    dispatch(registerAction(action));
 
-  return action();
-};
-
-export const saveNewTextFactory = (fetch: any) => (uri: string, item: ListItem, text: string) => (dispatch: Dispatch<IAction>) => {
-  const { id } = item;
-  const updatedItem = {
-    text,
-    isBeingEdited: false,
-    id,
+    return action();
   };
 
-  const action = () => fetch(
-    uri + id,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
+export const saveNewTextFactory =
+  (fetch: any) =>
+  (saveItemChanges: (id: Guid, text: string) => IAction) =>
+  (notifySuccess: (message: string) => IAction) =>
+  (notifyError: (message: string) => IAction) =>
+  (registerAction: (action: () => void) => IAction) =>
+  (handleErrors: <T>(response: T) => T) =>
+  (uri: string, item: ListItem, text: string) =>
+  (dispatch: Dispatch<IAction>) => {
+    const { id } = item;
+    const updatedItem = {
+      text,
+      isBeingEdited: false,
+      id,
+    };
+
+    const action = () => fetch(
+      uri + id,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedItem),
       },
-      body: JSON.stringify(updatedItem),
-    },
-  )
-    .then(handleErrors)
-    .then(() => {
-      dispatch(notifySuccess('Item text was updated.'));
-      dispatch(saveItemChanges(item.id, text));
-    })
-    .catch(() => dispatch(notifyError('Item failed to update.')));
+    )
+      .then(handleErrors)
+      .then(() => {
+        dispatch(notifySuccess('Item text was updated.'));
+        dispatch(saveItemChanges(item.id, text));
+      })
+      .catch(() => dispatch(notifyError('Item failed to update.')));
 
-  dispatch(registerAction(action));
+    dispatch(registerAction(action));
 
-  return action();
-};
+    return action();
+  };
--- src/actions/thunk/index.ts
+++ src/actions/thunk/index.ts
@@ -3,12 +3,25 @@ import {
   deleteItemFactory, fetchItemsFactory, openItemFactory,
   postItemFactory, saveNewTextFactory,
 } from './actionCreatorsWithDependency';
+import {
+  addNewItem, cancelItemChanges, deleteItem,
+  fetchFailed, notifyError, notifySuccess, openItemForEditing, receiveItems, registerAction,
+  requestItems, saveItemChanges
+} from '../actionCreators';
 
-export const fetchItems = fetchItemsFactory(fetch);
-export const postItem = postItemFactory(fetch);
-export const deleteItemFromServer = deleteItemFactory(fetch);
-export const cancelItem = cancelItemFactory(fetch);
-export const openItem = openItemFactory(fetch);
-export const saveNewText = saveNewTextFactory(fetch);
+const handleErrors = (response: any) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  return response;
+};
+
+export const fetchItems = fetchItemsFactory(fetch)(requestItems)(receiveItems)(fetchFailed)(notifyError)(registerAction)(handleErrors);
+export const postItem = postItemFactory(fetch)(addNewItem)(notifySuccess)(notifyError)(registerAction)(handleErrors);
+export const deleteItemFromServer = deleteItemFactory(fetch)(deleteItem)(notifySuccess)(notifyError)(registerAction)(handleErrors);
+export const cancelItem = cancelItemFactory(fetch)(cancelItemChanges)(notifyError)(registerAction)(handleErrors);
+export const openItem = openItemFactory(fetch)(openItemForEditing)(notifyError)(registerAction)(handleErrors);
+export const saveNewText = saveNewTextFactory(fetch)(saveItemChanges)(notifySuccess)(notifyError)(registerAction)(handleErrors);
 
 export * from './actionCreatorsWithDependency';
