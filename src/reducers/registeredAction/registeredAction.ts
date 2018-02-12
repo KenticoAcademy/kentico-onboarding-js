96866d2293f82c3b66e127bbf8682426cf25b74e
Radoslav Karlik <RadoslavK@kentico.com>
2018-02-12 12:51:39 +0100

    KA-259 Simplify reducer

--- src/reducers/registeredAction/registeredAction.ts
+++ src/reducers/registeredAction/registeredAction.ts
@@ -1,12 +1,10 @@
 import { IAction } from '../../models/interfaces/IAction';
 import { REGISTER_ACTION } from '../../constants/actionTypes';
 
-const registerAction = (action: IAction) => action.payload.action;
-
 export const registeredAction = (state: () => void = () => null, action: IAction): () => void => {
   switch (action.type) {
     case REGISTER_ACTION:
-      return registerAction(action);
+      return action.payload.action;
 
     default:
       return state;
