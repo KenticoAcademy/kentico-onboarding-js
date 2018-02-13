4e841a462271d929c9dc2b39620b9ea0251ea73e
VaclavM <vaclavm@kentico.com>
2018-02-13 10:07:45 +0100

    KA-266 Refactor NewItem and add prop-types
    Using prop-types library to validate props. Update styling in NewItem. Add functionality for Escape key.

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -1,32 +1,63 @@
+// components/NewItem.jsx
+
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class NewItem extends React.PureComponent {
+  static displayName = 'NewItem';
+
+  static propTypes = {
+    onCreate: PropTypes.func.isRequired,
+  };
+
   constructor(props) {
     super(props);
-    this.state = { item: '' };
+    this.state = {
+      itemValue: '',
+    };
   }
 
-  handleChange = (event) => this.setState({ item: event.target.value });
+  handleChange = (event) => this.setState({ itemValue: event.target.value });
 
   handleKeyUp = (event) => {
-    if (event.key === 'Enter' && this.state.item) {
+    if (event.key === 'Enter' && this.state.itemValue) {
       this.addItem();
+    }
+    else if (event.key === 'Escape') {
+      this.setState({ itemValue: '' });
     }
   };
 
   addItem = () => {
-    this.props.onCreate(this.state.item);
-    this.setState({ item: '' });
+    this.props.onCreate(this.state.itemValue);
+    this.setState({ itemValue: '' });
   };
 
   render() {
+    const { itemValue } = this.state;
+
     return (
       <div className="input-group">
-        <input type="text" className="form-control" placeholder="What is on your mind ... ?" value={this.state.item} onChange={this.handleChange} onKeyUp={this.handleKeyUp} />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="What is on your mind ... ?"
+          value={itemValue}
+          onChange={this.handleChange}
+          onKeyUp={this.handleKeyUp}
+        />
         <span className="input-group-btn">
-          <button type="button" className="btn btn-default" onClick={this.addItem} disabled={!this.state.item}>Add</button>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={this.addItem}
+            disabled={!itemValue}
+          >
+            Add
+          </button>
         </span>
       </div>
     );
   }
 }
+
