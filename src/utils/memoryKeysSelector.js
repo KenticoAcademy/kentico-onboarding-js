a2d5883775beb304fc3e367ba5a2bb7d07c9b17a
Vaclav Malinak <vaclavm@kentico.com>
2018-03-13 13:39:36 +0100

    KA-268 Import fast-memoize using normal import

--- src/utils/memoryKeysSelector.js
+++ src/utils/memoryKeysSelector.js
@@ -1,4 +1,4 @@
-import { default as memoize } from 'fast-memoize';
+import memoize from 'fast-memoize';
 
 const memorized = memoize(seq => seq);
 export const selectKeys = (items) => memorized(items.keySeq());
