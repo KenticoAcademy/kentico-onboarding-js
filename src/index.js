cde78c20ba906086db174c8dc6764f4c8f42fbfd
Vaclav Malinak <vaclavm@kentico.com>
2018-03-19 10:21:16 +0100

    KA-268 Make Redux DevTools and Redux-logger work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.css';
@@ -9,7 +10,12 @@ import application from './reducers/index';
 
 import { App } from './components/App.jsx';
 
-const store = createStore(application);
+const store = createStore(
+  application,
+  compose(
+    applyMiddleware(logger),
+    window.devToolsExtension ? window.devToolsExtension() : f => f)
+);
 
 ReactDOM.render(
   <Provider store={store}>
