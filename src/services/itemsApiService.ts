20b63da43ec163aa1d743274c269649f7f7792ec
Vaclav Malinak <vaclavm@kentico.com>
2018-04-04 08:58:25 +0200

    KA-281 Create getItems method to api service

--- src/services/itemsApiService.ts
+++ src/services/itemsApiService.ts
@@ -1,3 +1,19 @@
 import { Promise } from 'es6-promise';
 
-export const getItems = (fetchService: (url: string) => Promise<string>) => null;
+import { ITEMS_API_URL } from '../constants/constants';
+
+const processResponse = (response: Response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+};
+
+export const getItems = (fetchService: (url: string) => Promise<Response>) => fetchService(ITEMS_API_URL)
+    .catch(error => {
+      throw new Error(error);
+    })
+    .then(response => {
+      return processResponse(response);
+    });
