a9e3f0ff534f6cf38e3ef3668bcce86eae4c3131
Linda Langerova <LindaL@kentico.com>
2018-04-16 17:31:04 +0200

    KA-280 Change EditedListItem structure

--- src/containers/EditedListItem.tsx
+++ src/containers/EditedListItem.tsx
@@ -1,29 +1,36 @@
-import { connect, Dispatch } from 'react-redux';
+import {
+  connect,
+  Dispatch
+} from 'react-redux';
 import { ComponentClass } from 'react';
-import { EditedListItem as EditedListItemComponent, IEditedListItemCallbackProps, IEditedListItemDataProps } from '../components/EditedListItem';
+import {
+  EditedListItem as EditedListItemComponent,
+  IEditedListItemCallbackProps,
+  IEditedListItemDataProps,
+  IEditListItemContainerProps
+} from '../components/EditedListItem';
 import { IAppState } from '../reducers/IAppState';
 import { IItem } from '../models/Item';
-import { ItemId } from '../models/ItemId';
-import { deleteItem, toggleEditing, updateItemText, textUpdateChange } from  '../actions';
+import {
+  deleteItem,
+  toggleEditing,
+  updateItemText,
+  textUpdateChange
+} from '../actions';
 
-interface IEditListItemContainerProps {
-  itemId: ItemId;
-}
-
-function mapStateToProps(state: IAppState, { itemId }: IEditListItemContainerProps): IEditedListItemDataProps {
+function mapStateToProps(state: IAppState, {itemId}: IEditListItemContainerProps): IEditedListItemDataProps {
   const item: IItem = state.items.byId.get(itemId);
   return {
-    item: item,
-    textUpdate: item.textUpdate,
+    item,
   };
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<IAppState>, { itemId }: IEditListItemContainerProps): IEditedListItemCallbackProps => ({
+const mapDispatchToProps = (dispatch: Dispatch<IAppState>, {itemId}: IEditListItemContainerProps): IEditedListItemCallbackProps => ({
   onDelete: () => dispatch(deleteItem(itemId)),
   onCancel: () => dispatch(toggleEditing(itemId)),
   onSave: () => dispatch(updateItemText(itemId)),
   textUpdateChange: (text: string) => dispatch(textUpdateChange(itemId, text)),
 });
 
-export const EditedListItem: ComponentClass<IEditListItemContainerProps>  = connect(mapStateToProps, mapDispatchToProps)(EditedListItemComponent);
+export const EditedListItem: ComponentClass<IEditListItemContainerProps> = connect(mapStateToProps, mapDispatchToProps)(EditedListItemComponent);
 
