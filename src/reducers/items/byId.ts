edff2e75d8ab1ae111ee0fa600d9dc2e750024f4
Linda Langerova <LindaL@kentico.com>
2018-05-03 12:45:41 +0200

    KA-280 Update the shape of item and state

--- src/reducers/items/byId.ts
+++ src/reducers/items/byId.ts
@@ -17,6 +17,7 @@ export const byId: Reducer<OrderedMap<ItemId, Item>> = (state = DEFAULT_STATE, a
       return state.set(action.payload.id, new Item({
         id: action.payload.id,
         text: action.payload.text,
+        synchronized: action.payload.synchronized,
       }));
 
     case actionTypes.UPDATE_ITEM:
@@ -25,27 +26,35 @@ export const byId: Reducer<OrderedMap<ItemId, Item>> = (state = DEFAULT_STATE, a
         isBeingEdited: false,
       }));
 
-    case actionTypes.DELETE_ITEM: {
+    case actionTypes.DELETE_ITEM:
       return state.delete(action.payload.id);
-    }
 
-    case actionTypes.TOGGLE_EDITING: {
+    case actionTypes.TOGGLE_EDITING:
       return state.update(action.payload.id, (item) => item.with({
         isBeingEdited: !item.isBeingEdited,
         textUpdate: item.text,
       }));
-    }
 
-    case actionTypes.EDIT_ITEM_TEXT: {
+    case actionTypes.TOGGLE_SYNCHRONIZED:
+      return state.update(action.payload.id, (item) => item.with({
+        synchronized: action.payload.synchronized,
+      }));
+
+    case actionTypes.EDIT_ITEM_TEXT:
       return state.update(action.payload.id, (item) => item.with({
         textUpdate: action.payload.updatedText,
       }));
-    }
 
     case actionTypes.RECEIVE_ITEMS: {
-      if(!action || !action.payload || !action.payload.items ) return state;
+      if (!action || !action.payload || !action.payload.items ) return state;
       const itemsArray = action.payload.items.map((item: any) => [[item.Id].toString(), new Item({ id: item.Id, text: item.Text})]);
       return OrderedMap(itemsArray);
+    }
+
+    case actionTypes.REQUEST_FAILED_FOR_ITEM: {
+      return state.update(action.payload.id, (item) => item.with({
+        errorMessage: action.payload.errorMessage,
+      }));
     }
 
     default:
--- src/reducers/items/status.ts
+++ src/reducers/items/status.ts
@@ -5,15 +5,13 @@ import { Reducer } from 'redux';
 export type statusType = {
   isFetching: boolean,
   didInvalidate: boolean,
-  lastUpdated: any,
   errorMessage: string,
-}
+};
 
 const DEFAULT_VALUE: statusType = {
   isFetching: false,
   didInvalidate: false,
-  lastUpdated: undefined,
-  errorMessage: "",
+  errorMessage: '',
 };
 
 export const status: Reducer<statusType> = (state = DEFAULT_VALUE, action: IAction) => {
@@ -22,23 +20,20 @@ export const status: Reducer<statusType> = (state = DEFAULT_VALUE, action: IActi
       return {
         isFetching: true,
         didInvalidate: false,
-        lastUpdated: state.lastUpdated,
-        errorMessage: "",
+        errorMessage: '',
       };
 
     case actionTypes.RECEIVE_ITEMS:
       return {
         isFetching: false,
         didInvalidate: false,
-        lastUpdated: action.payload.receivedAt,
-        errorMessage:"",
+        errorMessage: '',
       };
 
     case actionTypes.REQUEST_FAILED:
       return {
         isFetching: false,
         didInvalidate: false,
-        lastUpdated: state.lastUpdated,
         errorMessage: action.payload.errorMessage,
       };
 
