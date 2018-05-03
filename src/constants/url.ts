6972021fd77d2af1ce6376cb594b837610af6745
Linda Langerova <LindaL@kentico.com>
2018-05-03 12:51:18 +0200

    KA-280 Decompose url and add allerts

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,8 +1,8 @@
-const SUBDOMAIN = "";
-const DOMAIN = "localhost";
-const PORT = "54816";
-const API = "/api/v1";
-const ENDPOINT = "/itemlist";
+const SUBDOMAIN = '';
+const DOMAIN = 'localhost';
+const PORT = ':54816';
+const API = '/api/v1';
+const ENDPOINT = '/itemlist';
 
 export const getApiUrl = (): RequestInfo =>
-  "http://" + SUBDOMAIN + "." + DOMAIN + ":" + PORT + API + ENDPOINT;
+  'http://' + SUBDOMAIN  + DOMAIN + PORT + API + ENDPOINT;
---
+++ src/utils/assertAlert.ts
@@ -0,0 +1,18 @@
+import Alert from 'react-s-alert';
+
+const options = {
+  position: 'top-right',
+  effect: 'stackslide',
+  timeout: 3000
+};
+
+export function assertAlert (type: string, message: string) {
+  switch (type) {
+    case 'ERROR':
+      return Alert.error(message, options);
+    case 'SUCCESS':
+      return Alert.success(message, options);
+    default:
+      return -1;
+  }
+}
