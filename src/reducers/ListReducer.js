4e4770ddd1619c8373302aaa8c3c74ffc41651e1
Hana Navratilova <HanaN@kentico.com>
2018-07-20 11:51:49 +0200

    KA-321 create simple redux store

---
+++ src/reducers/ListReducer.js
@@ -0,0 +1,8 @@
+import * as Immutable from 'immutable';
+
+export const listReducer = (state = new Immutable.OrderedMap(), action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
