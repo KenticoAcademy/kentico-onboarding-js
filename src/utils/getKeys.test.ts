750fdcc48ef909aa50adc86b7c991873f8f9ac6b
Hana Navratilova <HanaN@kentico.com>
2018-08-14 12:45:29 +0200

    KA-322 add missing import to getKeys test

--- src/utils/getKeys.test.ts
+++ src/utils/getKeys.test.ts
@@ -1,9 +1,10 @@
 import { getKeys } from './getKeys';
 import { OrderedMap } from 'immutable';
+import { ListItem } from '../models/ListItem';
 
 describe('getKeys', () => {
   it('returns same reference for 2 consecutive calls with the same argument', () => {
-    const list = OrderedMap<string, IListItem>([
+    const list = OrderedMap<string, ListItem>([
       [123, undefined],
       [-489, undefined],
       [854, undefined]
@@ -18,12 +19,12 @@ describe('getKeys', () => {
   });
 
   it('only memoizes consecutive calls and does not fail when the number of arguments changes between calls', () => {
-    const list1 = OrderedMap<string, IListItem>([
+    const list1 = OrderedMap<string, ListItem>([
       [123, undefined],
       [-489, undefined],
       [854, undefined]
     ]);
-    const list2 = OrderedMap<string, IListItem>([
+    const list2 = OrderedMap<string, ListItem>([
       [123, undefined],
       [0, undefined]
     ]);
