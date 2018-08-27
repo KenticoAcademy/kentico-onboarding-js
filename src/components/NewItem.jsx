5df0915ed9a4907f5e376d9ae518e3a651cab979
Radek Pavelka <RadekP3@kentico.com>
2018-08-27 17:17:48 +0200

    KA-335 implement NewItem component

---
+++ src/components/NewItem.jsx
@@ -0,0 +1,45 @@
+import React, { PureComponent, } from 'react';
+import PropTypes from 'prop-types';
+
+export class NewItem extends PureComponent {
+  static displayName = 'NewItem';
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+
+  _changeInput = e => this.setState({ value: e.target.value });
+
+  _addItem = () => {
+    this.props.onAdd(this.state.value);
+    this.setState(() => ({ value: '' }));
+  };
+
+  render() {
+    return (
+      <div className="form-inline">
+        <div className="form-group">
+          <input
+            className="form-control"
+            value={this.state.value}
+            onChange={this._changeInput}
+            autoFocus
+          />
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={this._addItem}
+          >Add
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+NewItem.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
