74994a9829e27b2b5ac68793f8176a6182bc9e2a
Radek Pavelka <RadekP3@kentico.com>
2018-08-27 17:18:58 +0200

    KA-335 implement Item

---
+++ src/components/Item.jsx
@@ -0,0 +1,64 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { EditableItem } from './EditableItem';
+import { StaticItem } from './StaticItem';
+
+export class Item extends PureComponent {
+  static displayName = 'Item';
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inEdit: false,
+    };
+  }
+
+  _clickLabel = () => this.setState(() => ({
+    inEdit: true,
+  }));
+
+  _cancelEdit = () => this.setState(() => ({
+    inEdit: false,
+  }));
+
+  _saveItem = (value) => {
+    this.props.onEdit(this.props.item.id, value);
+    this.setState(() => ({ inEdit: false }));
+  };
+
+  _deleteItem = () => this.props.onDelete(this.props.item.id);
+
+  render() {
+    const value = this.props.item.value;
+
+    if (this.state.inEdit) {
+      return (
+        <EditableItem
+          value={value}
+          index={this.props.index}
+          onEdit={this._saveItem}
+          onDelete={this._deleteItem}
+          onCancel={this._cancelEdit}
+          autoFocus
+        />);
+    }
+    return (
+      <StaticItem
+        onClick={this._clickLabel}
+        index={this.props.index}
+        value={value}
+        autoFocus
+      />
+    );
+  }
+}
+
+Item.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired
+  }),
+  onEdit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired
+};
