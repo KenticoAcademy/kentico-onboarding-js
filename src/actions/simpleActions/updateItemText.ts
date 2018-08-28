862262010be780ca43ec462f2e14a9522434d75b
lindalangerova <LindaL@kentico.com>
2018-08-28 08:46:56 +0200

    KA-280 Remove unused actions and move these these are used only in thunk actions there

--- src/actions/simpleActions/updateItemText.ts
+++
@@ -1,8 +0,0 @@
-import { actionTypes } from '../../constants/actionTypes';
-import { ItemId } from '../../models/ItemId';
-import { IAction } from '../IAction';
-
-export const updateItemText = (id: ItemId): IAction => ({
-  type: actionTypes.UPDATE_ITEM,
-  payload: {id},
-});
