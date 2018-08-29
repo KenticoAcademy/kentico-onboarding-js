72cb4467e96fc6c054cd64818b52fb876b2d70c9
Radek Pavelka <RadekP3@kentico.com>
2018-08-29 13:47:10 +0200

    KA-335 change tests according to AAA syntax rules

--- src/utils/inputValidator.test.js
+++ src/utils/inputValidator.test.js
@@ -5,17 +5,20 @@ describe('Input validator', () => {
 
   it('empty string should return false', () => {
     const input = '';
-    expect(validateInput(input)).toBeFalsy();
+    const result = validateInput(input);
+    expect(result).toBeFalsy();
   });
 
   it('multiple whitespaces should return false', () => {
-    const input = '    ';
-    expect(validateInput(input)).toBeFalsy();
+    const input = '    '
+    const result = validateInput(input);
+    expect(result).toBeFalsy();
   });
 
 
   it('valid input should return true', () => {
     const input = 'hello';
-    expect(validateInput(input)).toBeTruthy();
+    const result = validateInput(input);
+    expect(result).toBeTruthy();
   });
 });
