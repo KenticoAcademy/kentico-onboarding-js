dd702335a6a801d5d09849ca5b440676cef59888
Radek Pavelka <RadekP3@kentico.com>
2018-08-29 14:33:51 +0200

    KA-335 use uuid package to generate IDs

--- src/utils/idGenerator.jsx
+++ src/utils/idGenerator.jsx
@@ -1,7 +1,5 @@
+const uuidv4 = require('uuid/v4');
+
 export function generateId() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+  return uuidv4();
 }
