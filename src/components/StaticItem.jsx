3c1b1b5cd19bc723795a5a03bb8450a9218042ef
Radek Pavelka <RadekP3@kentico.com>
2018-08-30 09:48:37 +0200

    KA-335 refactor StaticItem to stateless functional component

--- src/components/StaticItem.jsx
+++ src/components/StaticItem.jsx
@@ -1,21 +1,18 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class StaticItem extends PureComponent {
-  static displayName = 'NewItem';
+export const StaticItem = ({ onClick, index, value }) => {
+  return (
+    <div onClick={onClick}>
+      {index}. {value}
+    </div>
+  );
+};
 
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    index: PropTypes.number,
-    value: PropTypes.string.isRequired
-  };
+StaticItem.displayName = 'NewItem';
 
-
-  render() {
-    return (
-      <div onClick={this.props.onClick}>
-        {this.props.index}. {this.props.value}
-      </div>
-    );
-  }
-}
+StaticItem.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  index: PropTypes.number,
+  value: PropTypes.string.isRequired
+};
