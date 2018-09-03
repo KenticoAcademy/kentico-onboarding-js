05dab2e952806e71540aec8de53fb9142901ca73
Radek Pavelka <RadekP3@kentico.com>
2018-09-03 12:44:14 +0200

    KA-335 rename parameter and remove unnecessary preventDefault() method

--- src/components/StaticItem.jsx
+++ src/components/StaticItem.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const StaticItem = ({ item, onClick, index }) => {
-  const clickHandler = e => {
-    e.preventDefault();
+  const clickHandler = () => {
     onClick(item.id);
   };
 
