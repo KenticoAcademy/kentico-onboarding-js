068ca8d36ace9139d4a66323889496e7c7350e50
Radek Pavelka <RadekP3@kentico.com>
2018-09-04 16:05:16 +0200

    KA-337 rename const to better name

--- src/models/ListItem.jsx
+++ src/models/ListItem.jsx
@@ -1,9 +1,9 @@
 import { Record } from 'immutable';
 
-export const ListItem = new Record({
-  id: '00000000-0000-0000-0000-000000000000', // samé nuly
+const emptyItem = {
+  id: '00000000-0000-0000-0000-000000000000',
   text: '',
   isInEditMode: false
-  // vytvoř default (empty) item
-});
+};
 
+export const ListItem = new Record(emptyItem);
