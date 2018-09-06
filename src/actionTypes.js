bd8d252d8609e39a6899b110a57b8761cd9152fd
Radek Pavelka <RadekP3@kentico.com>
2018-09-06 17:17:58 +0200

    KA-339 create actionTypes.js

---
+++ src/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const START_EDIT = 'START_EDIT';
+export const CANCEL_EDIT = 'CANCEL_EDIT';
+export const SAVE_TODO = 'SAVE_TODO';
