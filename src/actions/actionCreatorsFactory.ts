81b26012a93d9ac543e495bc90685df97da82c16
Jaroslav Paral <jaroslavp@kentico.com>
2018-09-07 08:38:43 +0200

    KA-379 Add TypeScript's types to actions and generateId

--- src/actions/actionCreatorsFactory.ts
+++ src/actions/actionCreatorsFactory.ts
@@ -1,10 +1,11 @@
+import { Action } from './actionInterface';
 import { CREATE_ITEM } from './actionTypes';
 
-export const createItemFactory = generateId => text =>
+export const createItemFactory = (generateId: () => string) => (text: string): Action =>
   ({
     type: CREATE_ITEM,
     payload: {
       id: generateId(),
       text,
-    }
+    },
   });
