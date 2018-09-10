da595c7d3e52559ef3ae26da9835e2ac2ed5cfa7
lindalangerova <LindaL@kentico.com>
2018-09-10 16:32:08 +0200

    KA-280 Create actionFetchFactory

---
+++ src/utils/actionFetchFactory.ts
@@ -0,0 +1,33 @@
+import { ItemId } from '../models/ItemId';
+import { getApiUrl } from '../constants/url';
+import { requestMethodTypes } from '../constants/requestMethodTypes';
+import {
+  deleteFetch,
+  getFetch,
+  postFetch,
+  putFetch
+} from '../actions/applyActionFetch';
+
+const actionFetchFactory = (method: string): (id?: ItemId, text?: string) => Promise<Response> => {
+  const apiUrl: RequestInfo = getApiUrl();
+
+  switch (method) {
+    case requestMethodTypes.GET:
+      return getFetch(apiUrl) ;
+    case requestMethodTypes.POST:
+      return postFetch(apiUrl);
+    case requestMethodTypes.PUT:
+      return putFetch(apiUrl);
+    case requestMethodTypes.DELETE:
+      return deleteFetch(apiUrl);
+
+    default:
+      return Promise.reject;
+  }
+};
+
+const actionFetchFactoryWithErrorHandling = (method: string) => (id?: ItemId, text?: string): Promise<Response> =>
+  actionFetchFactory(method)(id, text)
+    .then(response => response.status >= 400 ? this.reject() : response);
+
+export {actionFetchFactoryWithErrorHandling as actionFetchFactory};
