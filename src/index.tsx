824cd92010cb6b0dd94c22d3be077efc2d37b8b0
Jaroslav Paral <jaroslavp@kentico.com>
2018-09-10 12:58:46 +0200

    KA-379 Transfer index.jsx, App.jsx to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 require.context('../public/', true);
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import ReactDom from 'react-dom';
-import React from 'react';
+import * as ReactDom from 'react-dom';
+import * as React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './utils/store';
-import { App } from './App.jsx';
+import { App } from './App';
 
 ReactDom.render(
   <Provider store={store}>
