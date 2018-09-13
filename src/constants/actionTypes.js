53d235709aee76df615120675af292c8008b2f7b
Radek Pavelka <RadekP3@kentico.com>
2018-09-13 17:08:22 +0200

    KA-339 rename actions and fix action creators

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,5 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
+export const ADD_ITEM = 'ADD_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
 export const START_EDIT = 'START_EDIT';
 export const CANCEL_EDIT = 'CANCEL_EDIT';
-export const SAVE_TODO = 'SAVE_TODO';
+export const SAVE_ITEM = 'SAVE_ITEM';
