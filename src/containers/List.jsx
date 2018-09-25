20d9a020e22f2050508a3c65b66e0d7fd961bcb5
Radek Pavelka <RadekP3@kentico.com>
2018-09-25 14:20:14 +0200

    KA-339 remove unneccesary method

--- src/containers/List.jsx
+++ src/containers/List.jsx
@@ -1,15 +1,10 @@
 import connect from 'react-redux/es/connect/connect';
 import { List } from '../components/List';
-import { addItem } from '../actions';
 import { memoizedIds } from '../selectors/itemSelector';
 
 const mapStateToProps = (state) => ({
   itemsIds: memoizedIds(state.items.keySeq())
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onAdd: (text) => dispatch(addItem(text))
-});
-
-const ConnectedList = connect(mapStateToProps, mapDispatchToProps)(List);
+const ConnectedList = connect(mapStateToProps)(List);
 export { ConnectedList as List };
