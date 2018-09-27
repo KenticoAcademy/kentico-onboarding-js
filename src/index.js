9033238314e4c2bbed9c412a459c132bab61d9bd
lindalangerova <LindaL@kentico.com>
2018-09-27 08:42:06 +0200

    KA-396 Rescue data-balloons and alerts thrown away

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ import { root } from './reducers/root.ts';
 import thunk from 'redux-thunk';
 
 import '.././css/style.css';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
+import 'balloon-css/balloon.css';
+
 
 const store = createStore(
   root,
