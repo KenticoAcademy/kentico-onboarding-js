e35f383e4d8014bc16b6624d1220577a67be99e1
lindalangerova <LindaL@kentico.com>
2018-10-02 16:05:39 +0200

    KA-396 Make buildable also at appVeyor by fixing tests

--- src/actions/IAction.ts
+++ src/actions/IAction.ts
@@ -1,6 +1,7 @@
 import { actionTypes } from '../constants/actionTypes';
+import { AnyAction } from 'redux';
 
-export interface IAction {
+export interface IAction extends AnyAction {
   type: actionTypes;
   payload?: any;
 }
--- src/actions/thunkActions/updateItem.test.ts
+++ src/actions/thunkActions/updateItem.test.ts
@@ -1,30 +1,28 @@
-import { actionTypes } from '../../constants/actionTypes';
 import { updateItem } from './updateItem';
-import { ItemId } from '../../models/ItemId';
-import { mockStore } from '../../../test/utils/mockStore';
+import { actionTypes } from '../../constants/actionTypes';
 
 describe('CreateUpdateItem', () => {
-  test('calls request, addItem and success action if the fetch response was successful', async () => {
+  it('calls request, addItem and success action if the fetch response was successful', async () => {
 
-    const fetch = jest.fn().mockImplementation((id: ItemId, text: string) => Promise.resolve(new Response('{"body":[{"Id":"' + id + '","Text":"' + text + '"}]}', {status: 200})));
+    const fetch = jest.fn();
+    const dispatch = jest.fn();
     const mockId = () => '42';
 
-    const store = mockStore();
-    await store.dispatch<any>(updateItem(fetch)(mockId(), 'Karel'));
-    const actions = store.getActions();
+    await updateItem(fetch)(mockId(), 'Karel')(dispatch);
 
-    expect(actions[0]).toHaveProperty('type', actionTypes.PRE_UPDATE_ITEM);
-    expect(actions[1]).toHaveProperty('type', actionTypes.TOGGLE_SYNCHRONIZED);
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch.mock.calls[0][0]).toHaveProperty('type', actionTypes.PRE_UPDATE_ITEM);
+    expect(dispatch.mock.calls[1][0]).toHaveProperty('type', actionTypes.UPDATE_SUCCEEDED);
   });
 
-  test('calls request and failed action if the fetch response was unsuccessful', async () => {
+  it('calls request and failed action if the fetch response was unsuccessful', async () => {
     const fetch = () => Promise.reject(Error);
     const mockId = () => '42';
 
-    const store = mockStore();
+    const dispatch = jest.fn();
     let rejected = '';
     try {
-      await store.dispatch<any>(updateItem(fetch)(mockId(), 'Karel'));
+      await updateItem(fetch)(mockId(), 'Karel')(dispatch);
     } catch (error) {
       rejected = error;
     }
