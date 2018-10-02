5489b0c80df40e8c554e7a1d99d6a3943e69c1c7
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-02 15:04:14 +0200

    KA-339 place basic action creators and factory function into seperate files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,47 +1,5 @@
-import {
-  ADD_ITEM,
-  CANCEL_EDIT,
-  DELETE_ITEM,
-  UPDATE_ITEM,
-  START_EDIT
-} from '../constants/actionTypes';
 import { generateId } from '../utils/idGenerator';
-
-export const addItemFactory = (generateIdFunction) => (text) => ({
-  type: ADD_ITEM,
-  payload: {
-    id: generateIdFunction(),
-    text,
-  }
-});
+import { addItemFactory } from './addItemFactory';
 
 export const addItem = addItemFactory(generateId);
-
-export const deleteItem = (id) => ({
-  type: DELETE_ITEM,
-  payload: {
-    id
-  }
-});
-
-export const updateItem = (id, text) => ({
-  type: UPDATE_ITEM,
-  payload: {
-    id,
-    text
-  }
-});
-
-export const startEdit = (id) => ({
-  type: START_EDIT,
-  payload: {
-    id
-  }
-});
-
-export const cancelEdit = (id) => ({
-  type: CANCEL_EDIT,
-  payload: {
-    id
-  }
-});
+export * from './actionCreators.js';
