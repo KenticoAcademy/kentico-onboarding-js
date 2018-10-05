3e805f717c2ad7448b14f81a941612a816d675fc
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-05 09:13:54 +0200

    KA-339 decompose initial state creation

--- src/index.jsx
+++ src/index.jsx
@@ -11,8 +11,9 @@ import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import { App } from './components/App.jsx';
 import { rootReducer } from './reducers/rootReducer';
-import { initialState } from './models/initialState';
+import { getDefaultItems } from './utils/getDefaultItems';
 
+export const initialState = { items: getDefaultItems() };
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
