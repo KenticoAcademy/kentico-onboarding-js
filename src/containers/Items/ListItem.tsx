b21b4bdc28cb0c3ca2d1351a22d8f60383e0768a
lindalangerova <LindaL@kentico.com>
2018-10-15 08:32:24 +0200

    KA-280 Move clicking to show item operations one div higher

--- src/containers/Items/ListItem.tsx
+++ src/containers/Items/ListItem.tsx
@@ -7,17 +7,8 @@ import {
 import { IAppState } from '../../reducers/IAppState';
 import { ItemId } from '../../models/ItemId';
 import { IAction } from '../../actions/IAction';
-import {
-  CreateRemoveItem,
-  CreateUpdateItem,
-  CreateUploadItemAgain
-} from '../../actions/index';
-import { resetItem } from '../../actions/simpleActions/resetItem';
-import { errorMessageTypes } from '../../constants/errorMessageTypes';
 import { Dispatch } from 'redux';
-import { assertAlert } from '../../utils/assertAlert';
-import { alertTypes } from '../../constants/alert/alertTypes';
-import { alertMessages } from '../../constants/alert/alertMessages';
+import {toggleEditing} from '../../actions/simpleActions/toggleEditing';
 
 export interface IListItemContainerProps {
   id: ItemId;
@@ -25,11 +16,7 @@ export interface IListItemContainerProps {
 }
 
 export interface IListItemCallbackProps {
-  onThrowAway: () => Promise<IAction>;
-  onSaveAgain: (text: string) => Promise<IAction>;
-  onUploadAgain: (text: string) => Promise<IAction>;
-  onRecover: () => IAction;
-  assertAlert: (type: alertTypes, message: alertMessages) => number;
+  onClick: () => IAction;
 }
 
 const mapStateToProps = (state: IAppState, {id, index}: IListItemContainerProps): IListItemDataProps => {
@@ -43,11 +30,7 @@ const mapStateToProps = (state: IAppState, {id, index}: IListItemContainerProps)
 };
 
 const mapDispatchToProps = (dispatch: Dispatch<IAction>, {id}: IListItemContainerProps): IListItemCallbackProps => ({
-  onThrowAway: () => dispatch(CreateRemoveItem(id)),
-  onSaveAgain: (text: string) => dispatch(CreateUpdateItem(id, text)),
-  onUploadAgain: (text: string) => dispatch(CreateUploadItemAgain(() => id)(text)),
-  onRecover: () => dispatch(resetItem(id, [errorMessageTypes.DELETE])),
-  assertAlert: (type, message) =>  assertAlert(type, message),
+  onClick: () => dispatch(toggleEditing(id)),
 });
 
 export const ListItem: ComponentClass<IListItemContainerProps> = connect(mapStateToProps, mapDispatchToProps)(ListItemComponent);
