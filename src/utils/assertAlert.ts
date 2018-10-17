0323b28c8c81132ad912c289416570fca88d5e11
lindalangerova <LindaL@kentico.com>
2018-10-17 23:37:55 +0200

    KA-280 Fix assertAlert to throw error on default and type check

--- src/utils/assertAlert.ts
+++ src/utils/assertAlert.ts
@@ -1,4 +1,6 @@
 import Alert from 'react-s-alert';
+import {alertMessages} from '../constants/alert/alertMessages';
+import {alertTypes} from '../constants/alert/alertTypes';
 
 const options = {
   position: 'top-right',
@@ -6,13 +8,13 @@ const options = {
   timeout: 3000
 };
 
-export function assertAlert(type: string, message: string) {
+export function assertAlert(type: alertTypes, message: alertMessages) {
   switch (type) {
     case 'ERROR':
       return Alert.error(message, options);
     case 'SUCCESS':
       return Alert.success(message, options);
     default:
-      return -1;
+      throw Error("Unknown alert type");
   }
 }
