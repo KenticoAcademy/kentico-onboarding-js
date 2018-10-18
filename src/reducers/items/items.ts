19f2b05d0a1ea901d9bf0e01f8aeec352c051f7b
lindalangerova <LindaL@kentico.com>
2018-10-18 12:42:53 +0200

    KA-280 Introduce new interface for items

--- src/reducers/items/items.ts
+++ src/reducers/items/items.ts
@@ -2,16 +2,14 @@ import {
   combineReducers,
   Reducer
 } from 'redux';
-import { OrderedMap } from 'immutable';
 import { newItemText } from './newItemText';
 import { byId } from './byId';
-import { Item } from '../../models/Item';
 import {
   status,
 } from './status';
-import { StatusType } from '../../models/Status';
+import {IItemsState} from '../IAppState';
 
-export const items: Reducer<OrderedMap<ItemId, Item> & string & StatusType> = combineReducers({
+export const items: Reducer<IItemsState> = combineReducers({
   byId,
   newItemText,
   status,
