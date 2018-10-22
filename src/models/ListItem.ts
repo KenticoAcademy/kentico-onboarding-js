9ff3b027d2b8b4266bc664b7ca3a8af9c12dcfc5
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-22 13:14:45 +0200

    KA-340 rewrite item model to typescript

--- src/models/ListItem.ts
+++ src/models/ListItem.ts
@@ -1,9 +1,27 @@
 import { Record } from 'immutable';
 
-const defaultListItem = {
+export interface IListItem {
+  id?: string;
+  text?: string;
+  isInEditMode?: boolean;
+}
+
+const defaultListItem: IListItem = {
   id: '00000000-0000-0000-0000-000000000000',
   text: '',
-  isInEditMode: false
+  isInEditMode: false,
 };
 
-export const ListItem = new Record(defaultListItem, 'ListItem');
+export class ListItem extends Record(defaultListItem, 'ListItem') implements IListItem {
+  readonly id: string;
+  readonly text: string;
+  readonly isInEditMode: boolean;
+
+  constructor(params?: Partial<IListItem>) {
+    params ? super(params) : super();
+  }
+
+  with(values: IListItem): ListItem {
+    return this.merge(values) as this;
+  }
+}
