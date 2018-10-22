20d0032209f2501f68c389bd0d003e64fc16ae50
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-22 13:42:10 +0200

    KA-340 rewrite reducers tests to typescript

--- src/reducers/todoList/items/itemTest.test.ts
+++ src/reducers/todoList/items/itemTest.test.ts
@@ -3,27 +3,28 @@ import { ListItem } from '../../../models/ListItem';
 import {
   startItemEditing,
   stopItemEditing,
-  updateItemText
-} from '../../../actions/index';
+  updateItemText,
+} from '../../../actions';
+import { IAction } from '../../../actions/IAction';
 
 describe('item reducer', () => {
   const itemId = '5';
-  const defaultItem = new ListItem({
+  const defaultItem: ListItem = new ListItem({
     id: itemId,
     isInEditMode: false,
     text: 'Lorem ipsum',
   });
 
-  const unknownAction = {
+  const unknownAction: IAction = {
     type: 'UNKNOWN_ACTION',
     payload: {
       text: 'Run the tests',
-      id: itemId
-    }
+      id: itemId,
+    },
   };
 
   it('should return the initial state with undefined state', () => {
-    const expectedState = ListItem();
+    const expectedState: ListItem = new ListItem();
 
     const actualState = item(undefined, unknownAction);
 
@@ -31,7 +32,7 @@ describe('item reducer', () => {
   });
 
   it('should return previous state on unknown action', () => {
-    const expectedState = defaultItem;
+    const expectedState: ListItem = defaultItem;
 
     const actualState = item(defaultItem, unknownAction);
 
@@ -39,7 +40,7 @@ describe('item reducer', () => {
   });
 
   it('should start edit mode of selected item when ITEM_START_EDITING action is dispatched', () => {
-    const expectedState = defaultItem.merge({ isInEditMode: true });
+    const expectedState: ListItem = defaultItem.with({isInEditMode: true});
 
     const actualState = item(defaultItem, startItemEditing(itemId));
 
@@ -47,8 +48,8 @@ describe('item reducer', () => {
   });
 
   it('should end edit mode of selected item when ITEM_STOP_EDITING action is dispatched', () => {
-    const defaultItemInEditMode = defaultItem.merge({ isInEditMode: true });
-    const expectedState = defaultItem.merge({ isInEditMode: false });
+    const defaultItemInEditMode: ListItem = defaultItem.with({isInEditMode: true});
+    const expectedState: ListItem = defaultItem.with({isInEditMode: false});
 
     const actualState = item(defaultItemInEditMode, stopItemEditing(itemId));
 
@@ -57,7 +58,7 @@ describe('item reducer', () => {
 
   it('should update selected item when ITEM_TEXT_UPDATE action is dispatched', () => {
     const newText = 'Buy Beer';
-    const expectedState = defaultItem.merge({ text: newText });
+    const expectedState: ListItem = defaultItem.with({text: newText});
 
     const actualState = item(defaultItem, updateItemText(defaultItem.id, newText));
 
--- src/reducers/todoList/items/itemsTest.test.ts
+++ src/reducers/todoList/items/itemsTest.test.ts
@@ -1,31 +1,32 @@
 import { OrderedMap } from 'immutable';
-import { items } from './items.js';
+import { items } from './items';
 import { ListItem } from '../../../models/ListItem';
 import { addItemFactory } from '../../../actions/addItemFactory';
-import { deleteItem } from '../../../actions/index';
+import { deleteItem } from '../../../actions';
+import { IAction } from '../../../actions/IAction';
 
 describe('items reducer', () => {
   const itemId = '5';
-  const defaultItems = OrderedMap([
+  const defaultItems: OrderedMap<Uuid, ListItem> = OrderedMap([
     [
       itemId,
       new ListItem({
         id: itemId,
         text: 'Buy Milk',
-      })
-    ]
+      }),
+    ],
   ]);
 
-  const unknownAction = {
+  const unknownAction: IAction = {
     type: 'UNKNOWN_ACTION',
     payload: {
       text: 'Run the tests',
-      id: itemId
-    }
+      id: itemId,
+    },
   };
 
   it('should return the initial state with undefined state', () => {
-    const expectedState = OrderedMap();
+    const expectedState = OrderedMap<Uuid, ListItem>();
 
     const actualState = items(undefined, unknownAction);
 
@@ -33,7 +34,7 @@ describe('items reducer', () => {
   });
 
   it('should return previous state on unknown action', () => {
-    const expectedState = defaultItems;
+    const expectedState = OrderedMap<Uuid, ListItem>(defaultItems);
 
     const actualState = items(defaultItems, unknownAction);
 
@@ -43,14 +44,14 @@ describe('items reducer', () => {
   it('should add new item to the store when ADD_ITEM action is dispatched', () => {
     const generateId = () => '5';
     const itemText = 'Run the tests';
-    const expectedState = OrderedMap([
+    const expectedState: OrderedMap<Uuid, ListItem> = OrderedMap([
       [
         itemId,
         new ListItem({
           id: itemId,
-          text: itemText
-        })
-      ]
+          text: itemText,
+        }),
+      ],
     ]);
 
     const actualState = items(OrderedMap(), addItemFactory(generateId)(itemText));
@@ -59,7 +60,7 @@ describe('items reducer', () => {
   });
 
   it('should delete selected item when ITEM_DELETE action is dispatched', () => {
-    const expectedState = OrderedMap();
+    const expectedState = OrderedMap<Uuid, ListItem>();
 
     const actualState = items(defaultItems, deleteItem(itemId));
 
