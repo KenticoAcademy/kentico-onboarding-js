ae5908330a82f5e681be299dbc5c193825ba5ba4
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-22 12:38:43 +0200

    KA-340 rewrite index to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
 require.context('../public/', true);
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import ReactDom from 'react-dom';
-import React from 'react';
+import * as ReactDom from 'react-dom';
+import * as React from 'react';
 import {
   applyMiddleware,
   createStore,
-  compose
+  compose,
 } from 'redux';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import { createInitialState } from './utils/createInitialState';
-import { App } from './components/App.tsx';
+import { App } from './components/App';
 import { rootReducer } from './reducers/rootReducer';
 
 export const initialState = createInitialState();
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(logger))
+  composeEnhancers(applyMiddleware(logger)),
 );
 
 ReactDom.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('app-root')
+  document.getElementById('app-root'),
 );
