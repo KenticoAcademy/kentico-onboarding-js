57d994078206cdfaf2834f796266edb3ccd11017
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-24 10:07:09 +0200

    KA-340 rewrite createInitialState to typescript

--- src/utils/createInitialState.ts
+++ src/utils/createInitialState.ts
@@ -1,7 +1,9 @@
-import { OrderedMap } from 'immutable';
+import { Iterable, OrderedMap } from 'immutable';
 import { getDefaultItems } from './getDefaultItems';
+import { ListItem } from '../models/ListItem';
+import { IStore } from '../store/IAppState';
 
-const mappedItems = getDefaultItems().map((item) => ([item.id, item]));
-const defaultItemsOrderedMap = OrderedMap(mappedItems);
-const initialState = { todoList: { items: defaultItemsOrderedMap } };
+const mappedItems = getDefaultItems().map((item: ListItem) => (Iterable<[string, ListItem]>([item.id, item])));
+const defaultItemsOrderedMap: OrderedMap<Uuid, ListItem> = OrderedMap(mappedItems);
+const initialState: IStore = {todoList: {items: defaultItemsOrderedMap}};
 export const createInitialState = () => initialState;
