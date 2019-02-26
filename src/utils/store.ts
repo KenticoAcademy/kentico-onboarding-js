25460e4d3b421ec1bbdb01b7e97fa7ad652c7827
Hana Navratilova <HanaN@kentico.com>
2019-02-26 10:23:25 +0100

    KA-328 enable Redux Thunk

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -5,24 +5,15 @@ import {
 } from 'redux';
 import { app } from '../reducers/app';
 import { createLogger } from 'redux-logger';
-import { getDefaultList } from './getDefaultList';
 import { IAppState } from '../reducers/interfaces/IAppState';
 import { IAction } from '../actions/IAction';
-import { ListSorting } from '../constants/ListSorting';
+import thunk from 'redux-thunk';
 
 const logger = createLogger({});
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const getPreloadedState = (): IAppState => ({
-  list: {
-    items: getDefaultList(),
-    sorting: ListSorting.CreatedTime,
-  }
-});
-
 export const store = createStore<IAppState, IAction, never, never>(
   app,
-  getPreloadedState(),
-  composeEnhancers(applyMiddleware(logger))
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
